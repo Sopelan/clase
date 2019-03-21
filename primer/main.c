d2ba7bac2a0f80af250f60e5c95934e34db97e2f
Sopelan <47353511+Sopelan@users.noreply.github.com>
2019-03-21 10:31:17 -0300

    Add files via upload

---
+++ primer/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int edad;
+    float altura;
+    altura=82.5;
+    edad=18;
+    printf("ingrese su edad\n");
+    scanf("%d",&edad);
+    printf("ingrese su altura");
+    scanf("%f",&altura);
+    printf("Hello world!\n");
+    printf("Marcos \t Sopelana\n");
+    printf("su edad es: %d",edad);
+    printf("\nsu altura es: %f",altura);
+
+}
