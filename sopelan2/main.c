a21c91063f0cf4ee81d053fbe59223f73fa54c68
Sopelan <47353511+Sopelan@users.noreply.github.com>
2019-03-22 10:33:58 -0300

    Add files via upload

---
+++ sopelan2/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+/*  int numero;
+    char letra;
+
+    printf("ingrese un numero: ");
+    scanf("%d",&numero);
+    printf("ingrese una letra: ");
+    fflush(stdin);
+    //setbuf(stdin,NULL)
+    scanf("%c", &letra);
+    printf("el numero es: %d\nla letra es: %c\n", numero , letra);*/
+    char palabra[10];
+    printf("ingrese una palabra: ");
+    scanf("%s", &palabra);
+    printf("la palabra es: %s" ,palabra);
+
+    system("pause");
+}
