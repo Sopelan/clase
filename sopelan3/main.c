907d66e1a7228f5508b97ca9961887f76dc2040e
sopelan <marcossopelana@gmail.com>
2019-03-28 10:54:08 -0300

    321323123

--- sopelan3/main.c
+++ sopelan3/main.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-int dividir(int numero);
-int esDistintoCero(int numero);
-int factorial(int numero);
-int cambiarvalor(int valor);
-int cambiarValorReferencia(int* valor);
+#include "bibloteca.h"
 int main()
 {
     /*int edad;
@@ -33,53 +29,4 @@ int main()
     printf("mi dato es : %d",midato);
     return 0;
 }
-int dividir(int numero)
-{
-    int respuesta;
-    int resultado;
-    respuesta=esDistintoCero(numero);
-    if(respuesta==1)
-    {
-        resultado =numero/2;
-    }
-    return respuesta;
-}
-int esDistintoCero(int numero)
-{
-    if(numero!=0)
-    {
-        return 1;
-    }
-    return 0;
-}
-int factorial(int numero)
-{
-    int respuesta;
-    if(numero==1)
-    {
-        return 1;
-    }
-    respuesta=numero*factorial(numero-1);
-    return respuesta;
-}
-int cambiarvalor(int valor)
-{
-    valor=999;
-    return 1;
-}
-int cambiarValorReferencia(int* valor)
-{
-    printf("el valor es: %d",*valor);
-    *valor=999;
-    return 1;
-}
-float dividirporReferencia(int datouno, int datodos , float *respuesta)
-{
-    if(datouno!=0)
-    {
-        *respuesta=(float)datouno/datodos;
-        return 1;
-    }
-    return 0;
 
-}
