854a9ab106e839f9160e99da2d723e1fceea7e9d
sopelan <marcossopelana@gmail.com>
2019-04-05 11:47:32 -0300

    Signed-off-by: sopelan <marcossopelana@gmail.com>

--- sopelan6.2/main.c
+++ sopelan6.2/main.c
@@ -4,10 +4,18 @@
 int main()
 {
     char nombre [50] ;
+    int largo;
+    int compara;
     printf("ingrese su nombre: \n");
-    //scanf("%s",nombre);
+    //scanf("%[^\n]",nombre);
+    //fgets(nombre , 50 , stdin)
     gets(nombre);
+    //largo = strlen(nombre);
     printf("Su nombre es : %s\n", nombre);
-
+    //printf("%d" , largo);
+    /*if(nombre == "pepe")
+        printf("usted se llama pepe");*/
+    compara = strcmp(nombre , "pepe");
+    printf("%d" , compara);
     return 0;
 }
