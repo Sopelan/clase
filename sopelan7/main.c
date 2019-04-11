0fb988c95722614a90209b543bf1f2213294e340
sopelan <marcosopelana@gmail.com>
2019-04-11 12:26:38 -0300

    Signed-off-by: sopelan <marcosopelana@gmail.com>

--- sopelan7/main.c
+++ sopelan7/main.c
@@ -39,3 +39,14 @@ void getString(char mensaje[], char input[])
     scanf("%s",input);
     return 0;
 }
+int soloLetras(char str[])
+{
+    int i = 0;
+    while(str[i] != '\0')
+    {
+        if((str[i] < 'a' || str[i] > 'z' ) && (str[i] < 'A' || str[i] > 'Z' ))
+            return 0;
+        i++;
+    }
+    return 1;
+}
