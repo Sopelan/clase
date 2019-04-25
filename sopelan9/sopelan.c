d6a1e9a9e5b1030d8b9b9570128e0d59d57138a2
sopelan <marcossopelana@gmail.com>
2019-04-25 12:14:28 -0300

    Signed-off-by: sopelan <marcossopelana@gmail.com>

--- sopelan9/sopelan.c
+++ sopelan9/sopelan.c
@@ -14,7 +14,8 @@ eProducto pedirProducto()
     fflush(stdin);
     gets(retorno.codigoDeBarra);
     printf("pedir precio\n");
-    scanf("%s",&retorno.precio);
+    fflush(stdin);
+    gets(retorno.precio);
     return retorno;
 }
 void mostraProducto(eProducto mostrar)
@@ -24,6 +25,7 @@ void mostraProducto(eProducto mostrar)
     printf("3. %s\n",&mostrar.codigoDeBarra);
     printf("4. %s\n",&mostrar.fechaDeVecimiento);
     printf("5. %d\n",atoi(mostrar.precio));
+    printf("6. %d\n",atoi(mostrar.estado));
 }
 void cargarArray(int tam ,eProducto listado[])
 {
@@ -39,5 +41,37 @@ void mostrarArray(int tam,eProducto listado[])
     for(i = 0 ; i < tam ; i++)
     {
         mostraProducto(listado[i]);
+    }
+}
+/*int insertProducto(eProducto,eProducto[]);
+int dameLugarLIbre(eProducto[]);
+int borrarProducto(eProducto elProducto,eProducto listado[])
+{
+    int indice;
+    indice = existeProducto(elProducto,listado);
+    if(listado != -1)
+    {
+        listado[indice].estado = -1;
+    }
+    return indice;
+}
+int existeProducto(eProducto elProducto,eProducto listado[]);
+
+int editarProducto(eProducto,eProducto[])
+{
+    int inice;
+    indice = dameLugarLIbre(listado);
+    if(listado != -1)
+    {
+        listado[indice] = elProducto;
+    }
+    return indice;
+}*/
+void cargarEnCero(eProducto listado[] , int tam)
+{
+    int i;
+    for(i = 0 ; i < tam ; i++)
+    {
+        listado[i].estado = '0';
     }
 }
--- sopelan9/sopelan.h
+++ sopelan9/sopelan.h
@@ -6,10 +6,20 @@ char nombre[50];
 char fechaDeVecimiento[50];
 char provedor[50];
 char precio[50];
+char estado[50];
 }eProducto;
 eProducto pedirProducto();
 void mostraProducto(eProducto);
 void cargarArray(int tam ,eProducto listado[]);
 void mostrarArray(int tam,eProducto listado[]);
+void construirArray(int tam , eProducto listado[]);
+int insertProducto(eProducto,eProducto[]);
+int dameLugarLIbre(eProducto[]);
+int existeProducto(eProducto,eProducto[]);
+int borrarProducto(eProducto,eProducto[]);
+int editarProducto(eProducto,eProducto[]);
+void cargarEnCero(eProducto[] , int);
+
+
 #include "sopelan.c"
 #endif // SOPELAN_H_INCLUDED
