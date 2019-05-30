b1ca4f50ebd4da863b0b494a9b0f5a4a1f95c1a0
sopelan <marcossopelana@gmail.com>
2019-05-30 13:38:02 -0300

    Signed-off-by: sopelan <marcossopelana@gmail.com>

--- sopelan19/main.c
+++ sopelan19/main.c
@@ -8,10 +8,11 @@ int main()
 {
     FILE *pArchivo;
     eAlumno alumno;
+    char texto[] = "lalala;555;10";
     strcpy(alumno.nombre ,"marcos");
-
     printf("%p\n",&pArchivo);
-    if(pArchivo = fopen(ARCHIVO,"a") == NULL)
+
+    if( (pArchivo = fopen(ARCHIVO,"w")) == NULL)
     {
         printf("el archivo no puede ser abierto\n");
         exit(1);
@@ -20,25 +21,28 @@ int main()
     {
         printf("archivo abierto\n");
         //fwrite(&alumno , sizeof (eAlumno) , 1 , pArchivo);
-        char texto[] = "lalala;555;10";
+
         int largo = strlen(texto);
         fwrite(texto , sizeof (char) , largo , pArchivo);
         fclose(pArchivo);
     }
 
-    if(pArchivo = fopen(ARCHIVO,"r") == NULL)
+    if((pArchivo = fopen(ARCHIVO,"r")) == NULL)
     {
         printf("el archivo no puede ser abierto\n");
         exit(1);
     }
     else
     {
+        char renglon[70];
         printf("archivo abierto\n");
         while(!feof(pArchivo))
         {
-
+            int cantidad = fread(renglon , sizeof(char),70 ,pArchivo);
+            printf("\ncantidad %d y texto %s\n",cantidad,texto);
         }
 
     }
+
     return 0;
 }
